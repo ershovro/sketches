6d99b99cc18df1f929149df341e88f3d86b2c802
ro.ershov <ro.ershov@tensor.ru>
2019-12-02 12:40:58 +0300

    combSort  selectionSort gnomeSort

--- Algorithms/sorting.js
+++ Algorithms/sorting.js
@@ -143,3 +143,109 @@ const insertionSort = (arr = []) => {
    
    return arr;
 }
+
+/*
+ Гномья сортировка
+ Сложность O(n^2)
+*/
+
+const gnomeSort = (arr = []) => {
+   for(let i = 1, length = arr.length; i < length; ++i) {
+      
+      for(let j = i; j > 0; --j) {
+         if (arr[j] < arr[j - 1]) {
+            [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];           
+         } else {
+            break;
+         }
+      }
+      
+   }
+   
+   return arr;
+}
+
+const gnomeSort = (arr = []) => {
+   let 
+      i = 1,
+      length = arr.length;
+   
+   while(i < length) {
+      if (i > 0 && arr[i - 1] > arr[i]) {
+         [arr[i - 1], arr[i]] = [arr[i], arr[i - 1]];
+         --i;
+      } else {
+         ++i;
+      }
+   }
+   
+   return arr;
+}
+
+
+/*
+ Сортировка выбором.
+ Сложность O(n^2)
+ Является неусточивой.
+ Отличие устойчивых сортировок от неустойчивых в том, 
+ что если сравниваются не просто значения,
+ а объекты по какому-то из свойств,
+ то устойчивая сортировка оставит объекты с одинаковым ключом в том же порядке, 
+ в котором они были, а неустойчивая перемешает.
+*/
+
+var selectionSort = (arr = []) => {
+   for(let i = 0, length = arr.length; i < length; ++i) {
+      let smallest = i;
+      
+      for(let j = i; j < length; ++j) {
+         if(arr[smallest] > arr[j]) {
+            smallest = j;
+         }
+      }
+      
+      if (smallest !==i) {
+         [arr[i], arr[smallest]] = [arr[smallest], arr[i]];
+      }
+   }
+   
+   return arr;
+}
+
+
+/*
+ Сортировка расческой.
+ Является неустойчивой.
+ Оптимизация пузырькойвой сортировки - также сравниваются элементы, но не всегда соседние,
+ а эл-ты на большем расстоянии.
+ Число, на которое должен раз за разом уменьшаться разрыв,
+ было названо фактором уменьшения и выведено авторами сортировки из формулы:
+ ( 1 / ( 1-e^(-φ) ) = 1.247330950103979
+*/
+
+const combSort = (arr = []) => {
+    const l = arr.length;
+    const factor = 1.247;
+    let gapFactor = l / factor;
+    
+    while (gapFactor > 1) {
+        const gap = Math.round(gapFactor);
+        
+        for (let i = 0, j = gap; j < l; i++, j++) {
+            if (arr[i] > arr[j]) {
+                [arr[i], arr[j]] = [arr[j], arr[i]];
+            }
+        }
+        
+        gapFactor = gapFactor / factor;
+    }
+    return arr;
+};
+
+
+
+
+/*
+ Быстрая сортировка
+*/
+
